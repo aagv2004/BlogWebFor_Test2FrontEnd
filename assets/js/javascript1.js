9bb4c12f723302314aadb1e46dfdae3c901f1b50
Alejandro González <andresg.vergara04@gmail.com>
2024-05-14 09:31:29 -0400

    detodounpoco
    wajajaja

--- assets/js/javascript1.js
+++ assets/js/javascript1.js
@@ -1,51 +1,79 @@
 window.addEventListener("load", () => {
     document.getElementById("btnValidar").addEventListener("click", validar);
-    document.getElementById("btnContraste").addEventListener("click", contraste);
+    // document.getElementById("btnContraste").addEventListener("click", contraste);
+    document.getElementById("btnContraste").addEventListener("click", activateClass);
+
 })
 
-function contraste(){
-    let eBody = document.body;
-    let colorBody = eBody.style.backgroundColor;
-    let eH1 = document.getElementsByClassName("textoH1"); //esto tiene elementos
-    let eInputs = document.getElementsByTagName("input");
-    let eTextAreas = document.getElementsByTagName("textarea");
-    console.log(eInputs);
-    console.log(eInputs[1]);
-    console.log(...eInputs,...eTextAreas)
-    if (colorBody === "black") {
-        eBody.style.backgroundColor = "white";
-        for (let index = 0; index < eH1.length; index++) {
-            const element = eH1[index];
-            element.style.color="black";
-        }
-        for (let index = 0; index < eInputs.length; index++) {
-            const element = eInputs[index];
-            element.style.borderColor="black"
-        }
-        for (let index = 0; index < eTextAreas.length; index++) {
-            const element = eTextAreas[index];
-            element.style.borderColor="black"
-        }
-        // eH1.style.color ="black"
-    } else {
-        eBody.style.backgroundColor = "black";
-        for (let index = 0; index < eH1.length; index++) {
-            const element = eH1[index];
-            element.style.color="white";
-        }
-        for (let index = 0; index < eInputs.length; index++) {
-            const element = eInputs[index];
-            element.style.borderColor="white"
-        }
-        for (let index = 0; index < eTextAreas.length; index++) {
-            const element = eTextAreas[index];
-            element.style.borderColor="white"
-        }
-        // eH1.style.color="white"
-    }
+function activateClass() {
+    toggleClasses("cuerpo", "bodyColor");
+    toggleClasses("mensaje", "textareaColor");
+    toggleClasses("riotid", "inputColor");
+    toggleClasses("telefono", "inputColor");
+    toggleClasses("instagram", "inputColor");
+    toggleClasses("rango", "colorRango");
+    toggleClasses("btnValidar", "colorEnviar");
+    toggleClasses("menuHorizontal", "colorMenu");
+    toggleClasses("principal", "pPrincipal");
+    toggleClasses("informacion", "pInformacion");
+    toggleClasses("imagenes", "pImagenes");
+    toggleClasses("video", "pVideo");
+    toggleClasses("formulario", "pFormulario");
+}
+
+function removeClasses (idElemento, claseElemento) {
+    element = document.getElementById(idElemento);
+    element.classList.add(claseElemento);
+}
+
+function addClasses(idElemento, claseElemento) {
+    element = document.getElementById(idElemento);
+    element.classlist.add(claseElemento);
+}
 
 
-    }
+function toggleClasses (idElemento, claseElemento) {
+    element = document.getElementById(idElemento);
+    element.classList.toggle(claseElemento);
+}
+
+
+
+// function contraste(){
+//     let eBody = document.body;
+//     let colorBody = eBody.style.backgroundColor;
+//     let eH1 = document.getElementsByClassName("textoH1"); //esto tiene elementos
+//     let eInputs = document.getElementsByTagName("input");
+//     let eTextAreas = document.getElementsByTagName("textarea");
+//     if (colorBody === "black") {
+//         eBody.style.backgroundColor = "white";
+//         for (let index = 0; index < eH1.length; index++) {
+//             const element = eH1[index];
+//             element.style.color="black";
+//         }
+//         for (let index = 0; index < eInputs.length; index++) {
+//             const element = eInputs[index];
+//             element.style.borderColor="black"
+//         }
+//         for (let index = 0; index < eTextAreas.length; index++) {
+//             const element = eTextAreas[index];
+//             element.style.borderColor="black"
+//         }
+//     } else {
+//         eBody.style.backgroundColor = "black";
+//         for (let index = 0; index < eH1.length; index++) {
+//             const element = eH1[index];
+//             element.style.color="white";
+//         }
+//         for (let index = 0; index < eInputs.length; index++) {
+//             const element = eInputs[index];
+//             element.style.borderColor="white"
+//         }
+//         for (let index = 0; index < eTextAreas.length; index++) {
+//             const element = eTextAreas[index];
+//             element.style.borderColor="white"
+//         }
+//     }
 
 function validar(){
     validarVacio("riotid");
